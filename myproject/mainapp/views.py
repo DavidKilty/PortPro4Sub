c08eaeeecde7a47aad945fed3b36f96ef1da18ad
David Kilty <davidbkilty@gmail.com>
2024-11-26 16:59:35 +0000

    finsihed linking tipper list function to homepage of tippers

--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -24,7 +24,7 @@ def logintipper(request):
             elif user.user_type == 'tippee':
                 return redirect('tippee_homepage')
         else:
-            return render(request, 'logintipper.html', {'error': 'Invalid username or password'})
+            return render(request, 'logintipper.html', {'error': 'Invalid email or password'})
     return render(request, 'logintipper.html')
 
 
@@ -43,15 +43,13 @@ def logintippee(request):
             login(request, user)
             return redirect('tippee_homepage')
         else:
-            return render(request, 'logintippee.html', {'error': 'Invalid username or password'})
+            return render(request, 'logintippee.html', {'error': 'Invalid email or password'})
     return render(request, 'logintippee.html')
 
 
 @login_required
 def tippee_homepage(request):
-    if request.user.is_authenticated and request.user.user_type == 'tippee':
-        return render(request, 'tippeehomepage.html', {'tippee': request.user})
-    return redirect('home')
+    return render(request, 'tippeehomepage.html', {'tippee': request.user})
 
 
 def signuptipper(request):
@@ -90,7 +88,38 @@ class TipperDeleteView(DeleteView):
     def get_queryset(self):
         return User.objects.filter(user_type='tipper')
 
+@login_required
+def tippee_jar(request):
+    
+    tips = []  
+    return render(request, 'tippeejar.html', {'tips': tips})
+
+
+@login_required
+def tipper_jar(request):
+    
+    tips = []  
+    return render(request, 'tipperjar.html', {'tips': tips})
+
+
+@login_required
+def tipper_list(request):
+    return render(request, 'tipper_list.html')
+
+
+@login_required
+def tipper_update(request, pk):
+    return render(request, 'tipper_update.html')
+
+
+@login_required
+def delete_account(request):
+    if request.method == 'POST':
+        request.user.delete()
+        return redirect('home')
+    return render(request, 'tipper_confirm_delete.html')
 
 def logout_view(request):
     logout(request)
     return redirect('home')
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -14,5 +14,10 @@ urlpatterns = [
     path('tippee_homepage/', views.tipper_homepage, name='tippee_homepage'),
     path('tipper/<int:pk>/delete/', views.TipperDeleteView.as_view(), name='tipper_delete'),
     path('logout/', views.logout_view, name='logout'),
+     path('tippeejar/', views.tippee_jar, name='tippee_jar'),
+    path('tipperjar/', views.tipper_jar, name='tipper_jar'),
+    path('tipper_list/', views.tipper_list, name='tipper_list'),
+    path('tipper_update/<int:pk>/', views.tipper_update, name='tipper_update'),
+    path('delete_account/', views.delete_account, name='delete_account'),   
 ]
  
