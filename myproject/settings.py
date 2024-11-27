681a5f3fb5863bb96fc90e639fdd943ef69016d1
David Kilty <davidbkilty@gmail.com>
2024-11-27 18:31:08 +0000

    removed static file storage from settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,7 +40,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'myproject.urls'
